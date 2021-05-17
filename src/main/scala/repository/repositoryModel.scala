d6207c91d429b10bdce06c60be586a78ec567f61
MaryamIsoyeva <mariam1999@ukr.net>
2021-05-17 19:23:59 +0300

    Remove the now ignored directory

--- src/main/scala/repository/repositoryModel.scala
+++
@@ -1,9 +0,0 @@
-package repository
-
-import java.time.LocalDateTime
-
-import io.circe.Json
-
-case class Source(id: String, url: String, lastProcessedArticleUrl: String, lastProcessedArticleDate: LocalDateTime)
-
-case class Articles(id: Long, sourceId: String, url: String, title: String, publishDate: LocalDateTime, content: String)
